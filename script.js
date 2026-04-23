f10ae392d80a5a3ee4e77d004e0d2984742f7070
rugg122 <44012135+rugg122@users.noreply.github.com>
2026-04-23 12:40:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const messageEl = document.getElementById("love-message");
+
+if (messageEl) {
+  messageEl.textContent = "Quero que você saiba que eu te amo demais minha princesa. Tenho muito orgulho de você. Você é minha melhor companhia.";
+}
